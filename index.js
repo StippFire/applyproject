07976fc6553ea90108400e0045fefdbb9166770f
StippFire <janst1992@hotmail.com>
2022-06-09 22:04:45 +0200

    new shit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-test
+var express=require("express");
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var app=express();
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(bodyParser.json())
+
+app.listen(6969,()=>{console.log("appläuft")});
+
+app.post("/Invoices", (req, res, next) => {
+    console.log(req.body.bewerbungstext)
+    // hier pdf generieren
+    res.json(req.body.bewerbungstext);
+   });
